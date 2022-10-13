3a769573435dfb2f446e7ff44393fe1a5e59372e
Italo-O <italoramonrodrigues@gmail.com>
2022-10-13 14:55:32 -0300

    Commit da atividade 4

---
+++ resolucao-problemas/atividade4_uc8.js
@@ -0,0 +1,21 @@
+var idade = 21
+var palestrantes = ['Bil', 'FidoBil', 'MulherdoBil'];
+
+
+if(idade>=18){
+    console.log("Selecione o palestrante:")
+    console.log (palestrantes);
+} else {
+    console.log("Você precisa ser maior de 18 anos.")
+}
+Bil = 100
+FidoBil = 88
+MulherdoBil = 63
+
+let vagas = (FidoBil + 1)
+
+if(vagas<=100){
+    console.log("Cadastro concluido")
+}else{
+    console.log("Vagas esgotadas! Por favor, escolha outra palestra.")
+}
